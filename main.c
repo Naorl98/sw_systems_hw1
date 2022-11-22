30ef31dead6af54c17252fa31e62d56799aeb193
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-11-22 14:44:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,21 +11,23 @@ int main(){
         if(isArmstrong(i) == 1) 
             printf(" %d, " ,i);
     }
-     printf("The Palindromes are:");
+    printf("\n");
+    printf("The Palindromes are:");
     for(int i = start; i<=end;i++){
         if(isPalindrome(i) == 1) 
             printf(" %d, " ,i);
     }
+    printf("\n");
     printf("The Prime numbers are:");
     for(int i = start; i<=end;i++){
         if(isPrime(i) == 1) 
             printf(" %d, " ,i);
     }
-    printf("/n");
-     printf("The Strong numbers are:");
+    printf("\n");
+    printf("The Strong numbers are:");
     for(int i = start; i<=end;i++){
         if(isStrong(i) == 1) 
             printf(" %d, " ,i);
     }
-    printf("/n");
+    printf("\n");
 }  
