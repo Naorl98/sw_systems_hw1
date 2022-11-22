aff29a66b8c83f11fdc6a4b8d2e989d283640217
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-11-22 14:27:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,30 +3,29 @@
 
 int main(){
 
-    int start = 100;
-    int end = 1000;
-    printf("The Primes Numbers: =  ");
-    for(int i = start; i<=end;i++){
-        if(isPrime(i) == 1) 
-            printf("%d, " ,i);
-    }
-    printf("/n");
-     printf("The Strongs Numbers: =  ");
-    for(int i = start; i<=end;i++){
-        if(isStrong(i) == 1) 
-            printf("%d, " ,i);
-    }
-    printf("/n");
-     printf("The Armstrong Numbers: =  ");
+    int start;
+    int end;
+    scanf("%d %d", &start, &end);
+    printf("The Armstrong numbers are:");
     for(int i = start; i<=end;i++){
         if(isArmstrong(i) == 1) 
-            printf("%d, " ,i);
+            printf(" %d, " ,i);
     }
-    printf("/n");
-     printf("The Palindrome Numbers: =  ");
+     printf("The Palindromes are:");
     for(int i = start; i<=end;i++){
         if(isPalindrome(i) == 1) 
-            printf("%d, " ,i);
+            printf(" %d, " ,i);
     }
+    printf("The Prime numbers are:");
+    for(int i = start; i<=end;i++){
+        if(isPrime(i) == 1) 
+            printf(" %d, " ,i);
+    }
+    printf("/n");
+     printf("The Strong numbers are:");
+    for(int i = start; i<=end;i++){
+        if(isStrong(i) == 1) 
+            printf(" %d, " ,i);
+    }
+    printf("/n");
     
-}
